9197bbe0ff8616e46931f8d1eab04fedaf474948
TABISH KHAN <tabish003@hotmail.com>
2015-08-20 21:16:52 -0700

    refactored random generator into helper method

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'shotgun'
+# require 'pony'
 
 get '/' do 
   erb :welcome
@@ -9,13 +10,7 @@ get '/random_person_selector' do
   if params[:p_names]
     # get names
     @name=params[:p_names]
-    # turn to array
-    @name = @name.split(',')
-    #remove all white trailing and leading whitespaces 
-    @name = @name.map{|x|x.strip}
-    #remove all empty strings
-    @name = @name.reject{|x|x.empty?}
-    @winner = @name.sample
+    @name.pick_random
   else
     @winner = ""
   end
@@ -28,4 +23,24 @@ end
 
 get '/results' do 
   erb :results 
+  #Pony.mail(:to => 'tabish003@hotmail.com', :from => 'tabish003@hotmail.com', :subject => 'sinatra app', :body => 'Hello there. A Personality quiz was just completed by somebody')
 end
+
+get '/team_organizer' do 
+  erb :team_organizer
+  if params[:people_names]
+    @name = @name.s
+  end
+end
+
+
+def pick_random
+  # turn to array
+  @name = @name.split(',')
+  #remove all white trailing and leading whitespaces 
+  @name = @name.map{|x|x.strip}
+  #remove all empty strings
+  @name = @name.reject{|x|x.empty?}
+  @winner = @name.sample
+end
+
